559e2c202a37673179b129ff6c842ad25233e862
agrup <agustinrodriguez206@gmail.com>
2021-09-10 20:23:28 -0300

    firs commit

---
+++ 2021/TP-1-Vectores.py
@@ -0,0 +1,39 @@
+#                                           Universidad Nacional de Luján - Departamento de Ciencias Básicas
+#                                                               1074 - Programación I
+#                                                               Trabajo Práctico 1 (Vectores)
+
+# Si bien el lenguaje (Python) permite trabajar con otras estructuras de datos diferentes, respetaremos las condiciones que se indican a continuación.
+# Un vector debe ser:
+# 	Homogéneo: Todos sus elementos son del mismo tipo.
+# 	Estático: Debe tener un tamaño fijo al ser definido.
+# 	Indizado: Posee un índice para acceder en forma directa a un elemento en particular.
+
+# 	Para todos los ejercicios se debe realizar una función para la carga del vector, otra para mostrar el vector ya cargado y las que correspondan a los procesos que se solicitan. Todas deben ser funciones  separadas.
+# 	Se debe además incluir un menú que permita ejecutar cada una de las opciones 
+
+# Ejercicios
+
+# 1  Definir un vector que pueda contener 250 elementos.
+#       a   Cargar el vector con 250 números enteros entre -100 y 100.(utilizar random).
+#       b   Calcular la cantidad de números negativos, positivos y ceros que se encuentran en el vector.
+#       c   Mostrar por pantalla la información anterior, fuera de la función.
+#       d   Realizar un programa que encuentre el mayor de los valores cargados en el problema anterior.
+#       e   Mostrar por pantalla la información anterior, fuera de la función.
+#       f   Mostrar los elementos que ocupan las posiciones impares del vector y su suma dentro de la misma función.
+
+# 2  Definir un array para contener 10 elementos.
+#       a   Cargar el array con los apellidos de 10 alumnos.
+#       b   Escribir una función que reciba el vector y un apellido ingresado por teclado y retorne True si se encuentra en el vector y False en caso contrario.
+#       c   Mostrar por pantalla la información indicando si el apellido está o no dentro del vector.
+
+# 3  Definir un array para contener 5000 elementos.
+#       a   Cargar el array con 5000 edades (18 a 80) (random)
+#       b   CalcularRealizar una función que indique cual es la edad de mayor frecuencia y su frecuencia (la que más se repite y cuántas veces está).
+
+# 4  Definir un array para contener 40 elementos.
+#       a   Cargar el vector con 40 números entre -10 y 10
+#       b   EscribirMostrar por pantalla el vector.
+#       c   MostrarEscribir una función que acepte como parámetro el vector y a cada número negativo le sume 15 y reemplace el negativo por ese valor.
+#       d   RealizarMostrar por pantalla el vector modificado.
+#       e   MostrarEscribir una función que reciba el vector anterior, sin negativos y que ante la repetición de un número reemplace cada repetición por -1 (menos uno). El procedimiento debe retornar el vector modificado y la cantidad de veces que fue modificado.
+#       f   Mostrar por pantalla el vector modificado y la cantidad de veces que se modificó
